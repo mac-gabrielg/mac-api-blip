edffc6a252b7b7a2ecf7744b55083ab5936ffdda
Gabriel Garbes <gabriel.garbes@mac.com.br>
2023-01-04 15:33:01 -0300

    novo arquivo

---
+++ index2.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "API Intermediária MAC v1.0.0",
+  });
+});
+
+app.listen(3000);
